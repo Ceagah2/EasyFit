2d6e232ed6e79ecd5981b8c2d14d179b6171a799
Carlos Ceagah <carlosceagah@gmail.com>
2024-02-15 11:46:40 -0300

    feat: create initial home screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { styles } from './styles';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello Home</Text>
+    </View>
+  );
+}
