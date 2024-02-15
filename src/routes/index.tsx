b160a69ac6a29b0c59f66d6611e5bf6398a4627d
Carlos Ceagah <carlosceagah@gmail.com>
2024-02-15 11:46:32 -0300

    feat: create initial route

---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from '../screens/Home';
+
+
+const Router = () => {
+  const Stack = createNativeStackNavigator();
+  return(
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default Router
